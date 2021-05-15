58085a84ddc9e1cb1607ccff8ff22f13eb148ff4
unknown <igor.lirussi@studio.unibo.it>
2021-05-15 13:01:45 +0200

    Gradle Added

---
+++ build.gradle.kts
@@ -0,0 +1,24 @@
+plugins {
+    java
+    scala
+    application
+}
+
+repositories {
+    mavenCentral()
+}
+
+dependencies {
+    implementation("org.scala-lang:scala-library:2.13.5")
+    testImplementation("org.junit.jupiter:junit-jupiter-api:5.7.1")
+    testRuntimeOnly("org.junit.jupiter:junit-jupiter-engine:5.7.1")
+}
+
+application {
+    mainClass.set("Main")
+}
+
+tasks.named<Test>("test") {
+    useJUnitPlatform()
+}
+
